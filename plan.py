91311b20e70fd047b9c042d4e2a5c55ffba10ef5
krabay-hifly <krabay-hifly>
2024-01-19 22:07:37 +0000

    update plan (chunk result to be stored in ws)

--- plan.py
+++ plan.py
@@ -3,7 +3,7 @@
 # MAGIC ### Experiment with finetuning an OSS LLM on Hifly data
 # MAGIC
 # MAGIC 1. Take selected folders / files from gdrive, upload to AZ Blob (manual)
-# MAGIC 2. Chunk files (larger, gpt-3.5-turbo / gpt-4-turbo) store chunks on AZ Blob (DBX nb)
+# MAGIC 2. Load and chunk files (larger, gpt-3.5-turbo / gpt-4-turbo), store docs in Workspace (DBX nb)
 # MAGIC 3. Generate QA pairs for SFT, store train dataset in Workspace (DBX nb)
 # MAGIC 4. SFT selected OSS with train data (DBX nb)
 # MAGIC 5. Use finetuned model to answer questions about custom dataset
@@ -14,7 +14,3 @@
 # MAGIC - G:\.shortcut-targets-by-id\1H-lHHzp1rv3kGYKoaGXexCBqARJque3M\4_Teams\Advanced_Analytics_Team\Sales\Bemutatkozó anyagok\Hiflylabs_AA_Sales_Deck_20230124_MASTER
 # MAGIC
 # MAGIC This means this poc model will be able to answer questions about (most) colleagues CV's, basic company info (wifi password, 12 points) and the references of the advanced analytics team
-
-# COMMAND ----------
-
-
