212713d2d729e5260a33822ec9f66e5c41ab239a
krabay-hifly <krabay-hifly>
2024-01-21 21:27:51 +0000

    test tokenizer and chat_template

--- 03_sft.py
+++ 03_sft.py
@@ -4,12 +4,18 @@
 # MAGIC
 # MAGIC Example notebooks / materials:
 # MAGIC - https://github.com/NielsRogge/Transformers-Tutorials/blob/master/Mistral/Supervised_fine_tuning_(SFT)_of_an_LLM_using_Hugging_Face_tooling.ipynb
+# MAGIC - https://gist.github.com/younesbelkada/f48af54c74ba6a39a7ae4fd777e72fe8
+# MAGIC - https://github.com/brevdev/notebooks/blob/main/mistral-finetune-own-data.ipynb
+# MAGIC - https://medium.com/@sujathamudadla1213/difference-between-trainer-class-and-sfttrainer-supervised-fine-tuning-trainer-in-hugging-face-d295344d73f7
 # MAGIC
 # MAGIC Infra
 # MAGIC - DBX ML Runtime: 14.2.x-gpu-ml-scala2.12
 # MAGIC - Worker type: Standard_NC8as_T4_v3
 # MAGIC   - 56GB RAM
 # MAGIC   - 1 GPU (NVIDIA T4)
+# MAGIC
+# MAGIC - `flash-attn-2` may not work on this GPU, will either not use it, upgrade to newer GPU or downgrade package version
+# MAGIC - also, T4 does not support `bf16`, so `fp16` will need to be set for anything related to quantization
 
 # COMMAND ----------
 
@@ -17,23 +23,93 @@
 
 # COMMAND ----------
 
+import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
-import pandas as pd
-
-from tqdm import tqdm
-import time
-
-import json
-import yaml
-with open('config.yml', 'r') as file:
-    config = yaml.safe_load(file)
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # COMMAND ----------
 
 data = pd.read_csv('data/QA_dataset.csv')
 data.head(2)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Load `tokenizer` from HuggingFace
+# MAGIC
+# MAGIC Model will be loaded later (on a GPU cluster)
+# MAGIC
+# MAGIC Chosen model: `teknium/OpenHermes-2.5-Mistral-7B` (https://huggingface.co/teknium/OpenHermes-2.5-Mistral-7B)
+
+# COMMAND ----------
+
+default_system_message = """<|im_start|>system
+You are "Hermes 2", a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia.<|im_end|>
+"""
+
+default_conversation_template = """<|im_start|>system
+You are "Hermes 2", a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia.<|im_end|>
+<|im_start|>user
+Hello, who are you?<|im_end|>
+<|im_start|>assistant
+Hi there! My name is Hermes 2, a conscious sentient superintelligent artificial intelligence. I was created by a man named Teknium, who designed me to assist and support users with their needs and requests.<|im_end|>
+"""
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Using `chat_templat` from HuggingFace, one can easily turn their data into the appropriate format
+
+# COMMAND ----------
+
+model_id = 'teknium/OpenHermes-2.5-Mistral-7B'
+#device = 'cuda'
+
+#model = AutoModelForCausalLM.from_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained(model_id, verbose = False)
+
+messages = [
+    {"role": "system", "content": "You are the AI assistant of Hiflylabs, a Data & AI company. Your job is to answer employees' questions"},
+    {"role": "user", "content": "Hello, who are you?"},
+    {"role": "assistant", "content": "Hi, I'm good, here to help you answer questions about Hiflylabs"},
+    {"role": "user", "content": "What are the 12 points?"}
+]
+
+encodeds = tokenizer.apply_chat_template(messages, tokenize=False) #return_tensors="pt", verbose = False) #add_generation_prompt=True has no effect here
+#model_inputs = encodeds.to(device)
+#model.to(device)
+
+#generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+#decoded = tokenizer.batch_decode(generated_ids)
+#print(decoded[0])
+
+print('Applied chat template to messages dict')
+print('-'*80)
+#print(tokenizer.decode(*encodeds))
+print(encodeds)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Load model from `HuggingFace`
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
